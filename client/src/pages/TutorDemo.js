703e655ba72716001854da16d50a88909cebc7c9
Linda Martinez <lindanmjw@gmail.com>
2022-02-16 20:56:41 -0500

    pushing to see URL

--- client/src/pages/TutorDemo.js
+++ client/src/pages/TutorDemo.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import Card from 'react-bootstrap/Card'
 
 export default function TutorDemo() {
   const [subject, setSubject] = useState("none")
@@ -36,15 +37,15 @@ export default function TutorDemo() {
 }
 
 
-<Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-     NEED TUTOR NAME, DESCRIPTION 
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
+// {/* <Card style={{ width: '18rem' }}>
+//   <Card.Img variant="top" src="holder.js/100px180" />
+//   <Card.Body>
+//     <Card.Title>Card Title</Card.Title>
+//     <Card.Text>
+//      NEED TUTOR NAME, DESCRIPTION 
+//       Some quick example text to build on the card title and make up the bulk of
+//       the card's content.
+//     </Card.Text>
+//     <Button variant="primary">Go somewhere</Button>
+//   </Card.Body>
+// </Card> */}
