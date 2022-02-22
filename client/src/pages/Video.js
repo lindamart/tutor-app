92c1f97d0eef918ddfc2afd30730fef0aa102e5c
Adriana Broadnax <alwaysadrianna@gmail.com>
2022-02-22 15:37:41 -0500

    video.js

---
+++ client/src/pages/Video.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function SearchBar() {
+    return (
+        <div className="search">
+            <div className="Searching"></div>
+            <input type="text"></input>
+            <div className="searchPic"></div>
+            <div className="results"></div>
+
+        </div>
+    )
+}
+
+export default SearchBar
