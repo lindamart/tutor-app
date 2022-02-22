b37ab7b059686ab10f74f81bc57aaebd47b91489
Linda Martinez <lindanmjw@gmail.com>
2022-02-22 17:01:30 -0500

    starting video page

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,6 +1,8 @@
 const router=require("express").Router()
 const tutorRouter=require("./tutor")
+const videoRouter=require("./video")
 
 router.use("/tutor",tutorRouter)
+router.use("/video",videoRouter)
 
 module.exports=router
---
+++ server/routes/api/video.js
@@ -0,0 +1,15 @@
+const router = require("express").Router()
+const Video = require("../../models/Video")
+
+router.get("/", async (req, res) => {
+  const videos = await Video.find({})
+  res.json(videos)
+})
+
+router.get("/:subject", async (req, res) => {
+  const {subject} = req.params
+  const videos = await Video.find({subject})
+  res.json(videos)
+})
+
+module.exports = router
