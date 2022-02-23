dccdf66235fb136ccab597ff82b28417edb0f3cc
Linda Martinez <lindanmjw@gmail.com>
2022-02-23 12:21:57 -0500

    added game code need code check

---
+++ client/src/components/GameCard.js
@@ -0,0 +1,14 @@
+// import React from 'react'
+
+// export default function GameCard({ game }) {
+//   return (
+//     <div className='gameCard'>
+//       <div className='cardHeader'>
+//         <div>{video.title}</div>
+//       </div>
+//       <div className='cardBody'>{game.link}</div>
+//       <div className='cardFooter'>{game.subject}</div>
+//     </div>
+// ADDED CODE NEED CODE CHECK...CAN THERE BE A BUTTON FOR LINK
+//   )
+// }
