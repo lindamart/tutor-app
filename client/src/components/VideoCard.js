395bb065535dae671109a757626128ca6acf9810
Linda Martinez <lindanmjw@gmail.com>
2022-02-22 19:55:11 -0500

    added code for video card needs code check

---
+++ client/src/components/VideoCard.js
@@ -0,0 +1,14 @@
+// import React from 'react'
+
+// export default function VideoCard({ video }) {
+//   return (
+//     <div className='videoCard'>
+//       <div className='cardHeader'>
+//         <div>{video.title}</div>
+//       </div>
+//       <div className='cardBody'>{video.link}</div>
+//       <div className='cardFooter'>{video.subject}</div>
+//     </div>
+// ADDED CODE NEED CODE CHECK...CAN THERE BE A BUTTON FOR LINK
+//   )
+// }
